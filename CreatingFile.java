5aec18070dadc016d422dcab8f0da7ec447ca47c
Shraddha saini <shraddhasaini99@gmail.com>
2020-07-28 10:13:54 +0530

    Update CreatingFile.java

--- CreatingFile.java
+++ CreatingFile.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 File obj = new File("C:\\Users\\500062194\\Desktop\\javalab\\File1.txt");
 System.out.println("Creating a file");
 if(obj.File()){
-System.out.println("File available");
+System.out.println("File is available");
 }
 else{System.out.println("File available");}
 }
